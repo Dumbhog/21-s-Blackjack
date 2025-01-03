f179eac303caae9c6b0187c25815d876ea5412e8
Charlie Hog <161877981+Dumbhog@users.noreply.github.com>
2025-01-03 18:45:46 +0000

    Updated to V3.0

--- 21s.py
+++ 21s.py
@@ -2,12 +2,14 @@ from random import randint
 
 card_base = randint(1, 13)
 is_playing = 0 # This is the variable that determines if the game is being played or not
+chips = 20 # This is the amount of chips the player has
+bet_amount = 0  # This is the amount of chips the player is betting at a given time
 
 def play():  # Defines the starting of a new round
     global is_playing
-    begin_input = input("Would you like to begin a round? (y) ")
+    begin_input = input(f"Chips: {chips}. Would you like to begin a round? (y) ")
     if begin_input != "y":
-        print("Goodbye")
+        print("You didn't agree. Goodbye!")
         is_playing = False
     else:
         is_playing = True
@@ -19,13 +21,26 @@ def recieve_card():
     if 2 <= card_base <= 10:
         return card_base
     elif card_base == 1:
-        return 1  # Assuming Ace is 11
+        return 1  # Assuming Ace is 1
     else:
         return 10  # For J, Q, K
 
 def game_loop():  # This is the main game loop
     global is_playing
+    global chips
+    global bet_amount
     cards_value = 0  # Defining the value of all cards added outside the loop
+
+    bet_amount = int(input("How much would you like to bet? " ))  # This is the input for the bet amount
+    if isinstance(bet_amount, int) == False:
+        print("Please enter a valid number.")
+        is_playing = False
+    if bet_amount > chips:
+        print("You don't have enough chips to bet that amount.")
+        is_playing = False
+    else: 
+        chips -= bet_amount
+        
     while is_playing:
 
         draw_input = input("Press Enter to draw a card, press (e) to stand " )  # This is the input for drawing a card
@@ -46,7 +61,8 @@ def game_loop():  # This is the main game loop
             draw_input = 0
 
             if cards_value > 21:
-                print("You're Bust! The value of your cards is over 21.")
+                chips -= bet_amount
+                print(f"You're Bust! The value of your cards is over 21. chips: {chips}")
                 is_playing = False
 
 
@@ -56,20 +72,22 @@ def game_loop():  # This is the main game loop
             while dealer_cards < 17:
                 dealer_card_value = (recieve_card())
                 dealer_cards += dealer_card_value
-                print(f"The dealer's card value is {dealer_card_value}. Total value: {dealer_cards}")
+                print(f"The dealer drew a card worth {dealer_card_value}. Total value: {dealer_cards}")
             print(f"The dealer's final value is {dealer_cards}")
             if dealer_cards > 21:
-                print("The dealer is bust! You win!")
+                chips += (bet_amount * 2)
+                print(f"The dealer is bust! You win! chips: {chips}")
                 is_playing = False
             elif dealer_cards > cards_value:
-                print("The dealer wins!")
+                print(f"The dealer wins! chips: {chips}")
                 is_playing = False
             elif cards_value > dealer_cards:
-                print("You scored higher than the dealer! You win!")
+                chips += (bet_amount * 2)
+                print(f"You scored higher than the dealer! You win! chips: {chips}")
                 is_playing = False
             elif dealer_cards == cards_value:
-                print("Push! You both get your bet back.")
+                chips += bet_amount
+                print(f"Push! You both get your bet back. chips: {chips}")
                 is_playing = False
-                
 while True:
     play()
